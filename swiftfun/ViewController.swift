d4a52e4cd4eff91970273877cd984895874298e5
Unknown <shanmuka.sadhu@gmail.com>
2018-07-01 22:23:50 -0400

    Now you can add or subract
    By switching the switch you could chooser your choice of operation(add or subtract)!

--- swiftfun/ViewController.swift
+++ swiftfun/ViewController.swift
@@ -9,19 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
     var buttonCount=0
     @IBOutlet var myLabel: UILabel!
     
+    @IBOutlet var topTextField: UITextField!
+    
+    
+    @IBOutlet var bottomTextField: UITextField!
+    
+    
+    @IBOutlet var additionSwitch: UISwitch!
+    
     @IBAction func button(_ sender: Any) {
-        buttonCount=buttonCount+1
+        
+        let addition = additionSwitch.isOn
+        
+        if addition == true{
+            let sum = Double(topTextField.text!)!+Double(bottomTextField.text!)!
+            
+            myLabel.text = "The sum of \(topTextField.text!) and \(bottomTextField.text!) is  "+String(sum)
+        }
+        else{
+            let sum = Double(topTextField.text!)!-Double(bottomTextField.text!)!
+            
+            myLabel.text = "The difference of \(topTextField.text!) and \(bottomTextField.text!) is  "+String(sum)
+        }
+        
+        
+        
+        /*buttonCount=buttonCount+1
         if(buttonCount >= 10){
             view.backgroundColor=UIColor.green
             myLabel.text="You pressed this button 10 times"
         }
         else{
             let final = 10-buttonCount
-            print("You are \(final) presses away from 10")
+            myLabel.text="You are \(final) presses away from 10"
         }
+ */
+        
     }
     
 
